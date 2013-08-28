67036f4872c2971b7670eaeaaa02c516ac766da5
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2013-08-28 09:54:42 +0200

    moving uri escape method to inline

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -7,7 +7,7 @@ class Admin
 	end
 
 	def authenticate ( password )
-    RestClient.post(urlencode("http://#{BC_PLATFORM_HOST}:#{BC_PLATFORM_PORT}/beancounter-platform/rest/user/#{@username}/authenticate?apikey=#{get_apikey}"), { 
+    RestClient.post(URI::escape("http://#{BC_PLATFORM_HOST}:#{BC_PLATFORM_PORT}/beancounter-platform/rest/user/#{@username}/authenticate?apikey=#{get_apikey}"), { 
     	:password => password
     }) do |req, res, status| 
     	true if status.code == "200" # API RETURNS 200 ON SUCCESS ONLY
@@ -15,10 +15,6 @@ class Admin
 	end
 
 	private
-		def urlencode(str)
-			return URI::escape(str)
-		end
-
 		def get_apikey
 			return "4994d6ee-9566-4190-8b38-232ff7709ba9" # hardcoded apikey
 		end
