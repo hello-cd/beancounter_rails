611a5ec8c6e842acbef0f242a01efb466a929be0
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2013-08-28 16:40:10 +0200

    introducing new dashboard and overall refactoring

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,32 +1,32 @@
 class AdminController < ActionController::Base
 
-  def is_logged?
-  	!session[:admin].nil?
+  layout "admin"
+
+  def logout
+    session[:admin] = nil
+    redirect_to admin_login_url, :notice => 'You have been logged out!'
   end
 
   def login
-  	render 'admin/login', :layout => "admin"
+    return redirect_to admin_dashboard_url if logged?
   end
 
-  def logout
-  	session[:admin] = nil
-		redirect_to "/admin/login", :notice => 'You have been logged out!'
-	end
+  def dashboard
+    return redirect_to admin_error_url unless logged?
+  end
 
-	def main
-		return redirect_to "/admin/error" unless is_logged?
-		render "admin/main", :layout => "admin"
-	end
+  def auth
+    admin = Admin.new(params[:username])
+    if admin.authenticate(params[:password]) 
+      session[:admin] = admin
+      redirect_to admin_dashboard_url
+    else
+      redirect_to admin_login_url, :notice => 'Username or password do not match!'
+    end
+  end
 
-	def auth
-		return redirect_to "/admin/main" if is_logged?
-		admin = Admin.new(params[:username])
-  	if ( admin.authenticate(params[:password]) )
-  		session[:admin] = admin
-  		redirect_to "/admin/main"
-  	else
-			redirect_to "/admin/login", :notice => 'Username or password do not match!'
-  	end
-	end
-
+  private
+    def logged?
+      session[:admin].present?
+    end
 end
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,25 +1,23 @@
 class Admin
 
-	attr_accessor :username
+  attr_accessor :username, :api_key
 
-	def initialize ( username )
-		@username = username
-	end
+  def initialize (username)
+    @username = username
+    @apikey = "4994d6ee-9566-4190-8b38-232ff7709ba9"
+  end
 
-	def authenticate ( password )
-    RestClient.post(URI::escape("http://#{BC_PLATFORM_HOST}:#{BC_PLATFORM_PORT}/beancounter-platform/rest/user/#{@username}/authenticate?apikey=#{get_apikey}"), { 
-    	:password => password
-    }) do |req, res, status| 
-    	true if status.code == "200" # API RETURNS 200 ON SUCCESS ONLY
+  def authenticate (password)
+    return false unless admin?
+    RestClient.post(URI::escape("http://#{BC_PLATFORM_HOST}:#{BC_PLATFORM_PORT}/beancounter-platform/rest/user/#{@username}/authenticate?apikey=#{@apikey}"), { 
+      :password => password
+    }) do |req, res, status|
+      true if status.code == "200" # API return 200 on success only
     end
-	end
+  end
 
-	private
-		def get_apikey
-			return "4994d6ee-9566-4190-8b38-232ff7709ba9" # hardcoded apikey
-		end
-
-		def is_admin?
-			true # always true for now
-		end
+  private
+    def admin?
+      true
+    end
 end
