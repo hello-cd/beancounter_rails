cf8b5970909797485f90b398c85b4d6417e91984
Nicola Moretto <nicola.moretto@xpeppers.com>
2013-09-02 15:21:07 +0200

    Adding before filter to protect customers dashboard

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,16 +2,18 @@ class AdminController < ActionController::Base
 
   layout "admin"
 
+  before_filter :only_super_admin, :only => [:customers_dashboard]
+  before_filter :only_my_dashboard, :only => [:dashboard]
+
   def logout
-    session[:admin] = nil
+    session[:admin_id] = nil
     redirect_to admin_login_url, :notice => 'You have been logged out!'
   end
 
   def login
     if logged?
-      admin = session[:admin]
-      redirect_to admin.customer.super? ? admin_customers_dashboard_url :
-                                          admin_dashboard_url(admin.customer)
+      redirect_to current_admin.customer.super? ? admin_customers_dashboard_url :
+                                                  admin_dashboard_url(current_admin.customer)
     end
   end
 
@@ -24,11 +26,10 @@ class AdminController < ActionController::Base
   def auth
     admin = Admin.find_by_username(params[:username])
     if admin.present? && admin.authenticate(params[:password])
+      session[:admin_id] = admin.id
       if admin.customer.super?
-        session[:admin] = admin
         redirect_to admin_customers_dashboard_url
       else
-        session[:admin] = admin
         redirect_to admin_dashboard_url(admin.customer)
       end
     else
@@ -40,8 +41,26 @@ class AdminController < ActionController::Base
     @customers = Customer.find_all_by_super(false)
   end
 
+  helper_method :current_admin
+  def current_admin
+    @admin ||= Admin.find(session[:admin_id])
+  end
+
   private
+
     def logged?
-      session[:admin].present?
+      session[:admin_id].present?
+    end
+
+    def only_super_admin
+      if !logged? || !current_admin.customer.super?
+        redirect_to admin_login_url
+      end
+    end
+
+    def only_my_dashboard
+      if current_admin.customer_id != params[:id].to_i
+        redirect_to admin_login_url
+      end
     end
 end
