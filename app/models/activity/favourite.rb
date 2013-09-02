e1290f7169e08d26db5d344676b87578d7f9d3b9
Ivo von Putzer Reibegg <ivo.putzer@gmail.com>
2013-09-02 12:56:38 +0200

    fixing array of urls in favorites-activity

--- app/models/activity/favourite.rb
+++ app/models/activity/favourite.rb
@@ -4,7 +4,7 @@ class Activity::Favourite < Activity
   def initialize(json)
     super(json)
     begin
-      send("url=", json.fetch("object").fetch("link"))
+      send("url=", [json.fetch("object").fetch("link")])
     rescue Exception => e
       Rails.logger.error e.message
     end
