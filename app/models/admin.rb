8b037652b35767b952ee286a128a89c08f939cca
Nicola Moretto <nicola.moretto@xpeppers.com>
2013-09-02 14:29:13 +0200

    Adding customer name from params

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,10 +2,6 @@ class Admin < ActiveRecord::Base
   attr_accessible :username
   belongs_to :customer
 
-  def api_name
-    application_setting.api_name
-  end
-
   def authenticate (password)
     return false unless admin?
     RestClient.post(URI::escape("http://#{BC_PLATFORM_HOST}:#{BC_PLATFORM_PORT}/beancounter-platform/rest/user/#{username}/authenticate?apikey=#{customer.api_value}"), {
