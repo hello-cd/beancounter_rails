801f26618c1ef5af1cda0ed761d3fef9b3d1da49
Nicola Moretto <nicola.moretto@xpeppers.com>
2014-02-26 11:34:18 +0100

    changing ip blade for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,9 +14,9 @@ ssh_options[:forward_agent] = true
 ssh_options[:keys] = ["#{ENV['HOME']}/.ssh/deploy"]
 default_run_options[:pty] = true
 
-role :web, "10.0.1.250:2224"                         # Your HTTP server, Apache/etc
-role :app, "10.0.1.250:2224"                         # This may be the same as your `Web` server
-role :db,  "10.0.1.250:2224", :primary => true        # This is where Rails migrations will run
+role :web, "10.0.0.250:2224"                         # Your HTTP server, Apache/etc
+role :app, "10.0.0.250:2224"                         # This may be the same as your `Web` server
+role :db,  "10.0.0.250:2224", :primary => true        # This is where Rails migrations will run
 
 set :keep_releases, 5
 after "deploy:update", "deploy:cleanup"
